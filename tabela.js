fde9667be0fd9510a5f9ab5d6f702d009d3bd72f
joaoanitelli <133887229+joaoanitelli@users.noreply.github.com>
2024-03-05 18:41:26 -0300

    Add files via upload

--- tabela.js
+++ tabela.js
@@ -1,12 +1,23 @@
+var lista = [];
+
 function adicionar() {
     var nome = document.getElementById('nome').value;
-    var tabela = document.getElementById('tabela'); // Correção aqui
-    var qtdLinhas = tabela.rows.length;
-    var linha = tabela.insertRow(qtdLinhas); // Correção aqui
-
-    var cellNome = linha.insertCell(0);
-
-    cellNome.innerHTML = nome;
-
+    lista.push(nome);
+    ordenarLista();
+    atualizarTabela();
     document.getElementById('nome').value = '';
+}
+
+function ordenarLista() {
+    lista.sort(); //
+}
+
+function atualizarTabela() {
+    var tabela = document.getElementById('tabela').getElementsByTagName('tbody')[0];
+    tabela.innerHTML = "";
+    for (var i = 0; i < lista.length; i++) {
+        var linha = tabela.insertRow(i);
+        var cellNome = linha.insertCell(0);
+        cellNome.innerHTML = lista[i];
+    }
 }
